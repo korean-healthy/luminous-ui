f4f8375349766e44a3359cad666c052a7285192b
HelloWorld017 <deu07115@gmail.com>
2017-03-11 14:17:14 +0900

    Fixed font-related problem 2

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
 
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/dist/',
+		publicPath: '',
 		filename: '[name].bundle.js',
 		library: 'LuminousUI',
 		libraryTarget: 'umd'
